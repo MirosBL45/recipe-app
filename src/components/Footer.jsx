30e955691027b0cc4fa99b7d5b823d0c1f7ffd46
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-02-26 13:35:31 +0100

    small change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,7 @@ function Footer() {
   return (
     <footer>
       <p className="footer_description">
-        Motivated by{' '}
-        <a
-          href="https://www.youtube.com/c/DevEd/videos"
-          target="_blank"
-          rel="noreferrer"
-        >
-          developedbyed
-        </a>
-        , Coded by{' '}
+        Coded by{' '}
         <a href="https://github.com/MirosBL45" target="_blank" rel="noreferrer">
           Miroslav Jovic
         </a>
